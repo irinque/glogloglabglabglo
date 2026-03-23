2d7ff3640a4f3fe35c514d9404534234795889b0
irinque <matvey@2ch.ru>
2026-03-23 12:39:14 +0300

    Funny commit with substract

--- калькулятор.py
+++ калькулятор.py
@@ -1,5 +1,9 @@
 def add (a, b):
     return a + b
 
+def substract (a, b):
+    return a - b
+
 if __name__ == "__main__":
     print(f"Сумма 5 и 3 равна {add(5, 3)}")
+    print(f"Вычитание 5 и 3 равнё {substract(5, 3)}")
