dae0d642d311ea7407acb751f18b113651ce7193
irinque <matvey@2ch.ru>
2026-03-23 12:54:58 +0300

    add multiply func

--- калькулятор.py
+++ калькулятор.py
@@ -4,6 +4,10 @@ def add (a, b):
 def substract (a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
 if __name__ == "__main__":
     print(f"Сумма 5 и 3 равна {add(5, 3)}")
     print(f"Вычитание 5 и 3 равнё {substract(5, 3)}")
+    print(f"Произведение 5 и 3 равно {multiply(5, 3)}")
